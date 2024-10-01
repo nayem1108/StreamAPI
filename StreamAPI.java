c0b440b63b1756afd6dd4a42104d329ac08df4e5
Nayem Uddin <nayemuddincucse@gmail.com>
2024-10-02 02:16:47 +0600

    list on name created

---
+++ StreamAPI.java
@@ -0,0 +1,19 @@
+package StreamApi;
+
+import java.util.ArrayList;
+
+/**
+ * StreamAPI
+ */
+public class StreamAPI {
+
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Nayem");
+        names.add("Uddin");
+        names.add("Fahim");
+        names.add("Ahmed");
+
+    }
+}
