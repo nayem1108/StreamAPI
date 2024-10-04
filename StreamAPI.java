b5e16b210bf67a64e054a1c75cd07b887dd43d65
Nayem Uddin <nayemuddincucse@gmail.com>
2024-10-04 09:11:33 +0600

    Space removed

--- StreamAPI.java
+++ StreamAPI.java
@@ -6,7 +6,6 @@ import java.util.function.Consumer;
  * StreamAPI
  */
 public class StreamAPI {
-
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<>();
 
