facb18cd1f1d41a0e957195d0ee7ba7e345ecc5c
Nayem Uddin <nayemuddincucse@gmail.com>
2024-10-04 09:29:25 +0600

    filter method learn

--- EngineeringDigest.java
+++ EngineeringDigest.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class EngineeringDigest {
     public static void main(String[] args) {
@@ -13,8 +14,10 @@ public class EngineeringDigest {
         System.out.println(streamedList);
 
         // Stream Methods
-        // Filter
         List<Integer> numList = Arrays.asList(1, 2, 3, 4, 5, 6, 6, 4534, 434, 3453, 236, 456, 2236, 54432, 64);
         System.out.println(numList);
+        // Filter
+        List<Integer> filteredList = numList.stream().filter(a -> a % 2 == 0).collect(Collectors.toList());
+        System.out.println(filteredList);
     }
 }
